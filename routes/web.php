64e7a600e56120b2ec404331b95f9d5131628969
DamianoMura <mura.damiano14@gmail.com>
2025-12-27 23:56:08 +0100

    [L11] setup route dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware('auth', 'verified')
+    ->name('admin')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
+    });
 
-require __DIR__.'/auth.php';
+
+
+require __DIR__ . '/auth.php';
