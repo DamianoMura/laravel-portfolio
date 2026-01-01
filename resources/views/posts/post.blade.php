0428e4b3859c8660b90641deb94c85ad847b8a64
DamianoMura <mura.damiano14@gmail.com>
2026-01-01 16:51:06 +0100

    [L12] few styling tweaks

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -12,17 +12,20 @@
      
       {{ $post->id." / ".$post->count() }}
 
-      <a href="{{ $post->id < $post->count()-1 ? route('posts.show', $post->id + 1) : $post->count()-1 }}"> next -></a>
+      <a href="{{ $post->id < $post->count() ? route('posts.show', $post->id + 1) : $post->count() }}"> next -></a>
    
       
     </div>
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
         <div class="flex flex-col p-6 text-gray-900 dark:text-gray-100">
-          <h1 class="mb-4 text-2xl font-bold">Author : {{$post->author}}</h1>
-          <h2 class="text-xl font-semibold">{{ $post->title }}</h2>
+          <div class="flex items-center justify-between "> 
+            <h1 class="mb-4 text-2xl font-bold"><i class="text-3xl fa-regular fa-circle-user"></i>  {{$post->author}} </h1> <small>Published on: {{
+              $post->created_at->format('F j, Y') }}</small>
+          </div>
+          <h2 class="text-xl font-semibold text-center">{{ $post->title }}</h2>
           <p class="mt-4 text-gray-600">{{ $post->content }}</p> 
-          <small class="self-end">Published on: {{ $post->created_at->format('F j, Y') }}</small>
+          
 
           
 
