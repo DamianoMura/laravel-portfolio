e2b33e827cf827842f81d764eea2d3363923e8ba
DamianoMura <mura.damiano14@gmail.com>
2026-01-01 16:31:46 +0100

    [L12] pagination

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -6,12 +6,23 @@
   </x-slot>
 
   <div class="py-12">
+    <div class="flex content-center justify-between mx-auto mb-4 max-w-7xl sm:px-6 lg:px-8"> 
+     
+      <a href='{{ $post->id > 1 ? route('posts.show', $post->id - 1) : "#" }}' ><- prev  </a>
+     
+      {{ $post->id." / ".$post->count() }}
+
+      <a href="{{ $post->id < $post->count()-1 ? route('posts.show', $post->id + 1) : $post->count()-1 }}"> next -></a>
+   
+      
+    </div>
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-        <div class="p-6 text-gray-900 dark:text-gray-100">
-          <h1 class="mb-4 text-2xl font-bold">Post </h1>
+        <div class="flex flex-col p-6 text-gray-900 dark:text-gray-100">
+          <h1 class="mb-4 text-2xl font-bold">Author : {{$post->author}}</h1>
           <h2 class="text-xl font-semibold">{{ $post->title }}</h2>
-          <p class="mt-4 text-gray-600">{{ $post->body }}</p> 
+          <p class="mt-4 text-gray-600">{{ $post->content }}</p> 
+          <small class="self-end">Published on: {{ $post->created_at->format('F j, Y') }}</small>
 
           
 
